a7ab8fb7d092abfb28528f83569c571955fdcc3d
Kazi Redwan <105074708+redwa-n@users.noreply.github.com>
2024-02-15 16:33:06 +0600

    Add files via upload

---
+++ LAB 2/AT1.php
@@ -0,0 +1,11 @@
+<?php
+
+$length = 5;
+$width = 3;
+
+$area = $length * $width;
+$perimeter = 2 * ($length + $width);
+
+echo "Area of the rectangle: " . $area . " square units\n";
+echo "Perimeter of the rectangle: " . $perimeter . " units\n";
+?>
---
+++ LAB 2/AT3.php
@@ -0,0 +1,10 @@
+<?php
+
+$number = 15;
+
+if ($number % 2 == 0) {
+    echo "$number is even.\n";
+} else {
+    echo "$number is odd.\n";
+}
+?>
