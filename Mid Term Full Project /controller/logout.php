295ce6f9871ae66a3cb7cf8fac0725937b9131dd
Kazi Redwan <redwan.stu@gmail.com>
2024-04-01 14:03:14 +0600

    Delete Mid Term Full Project  directory

--- Mid Term Full Project /controller/logout.php
+++
@@ -1,4 +0,0 @@
-<?php 
-    setcookie('flag', 'abc', time()-10, '/');
-    header('location: ../view/login.php');
-?>
--- Mid Term Full Project /controller/passChange.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    echo "Password changed successfully.";
-    session_destroy();
-}
-?>
